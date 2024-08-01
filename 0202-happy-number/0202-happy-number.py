21472ada0a4b089f371ec779e6dae629d71c69e9
Viet Minh (Michael) Nguyen <nguyenvietminh812@gmail.com>
2024-08-01 01:14:44 -0400

    Time: 38 ms (50.68%), Space: 16.5 MB (24.82%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -1,7 +1,19 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        visited = set()
-        while n != 1 and n not in visited:
-            visited.add(n)
-            n = sum([int(i) ** 2 for i in str(n)])
-        return n == 1
+        if n == 1: 
+            return True
+
+        slow = n
+        fast = n
+
+        while True:
+            slow = self.sumSqrDigits(slow)
+            fast = self.sumSqrDigits(self.sumSqrDigits(fast))
+
+            if slow == fast:
+                break
+            
+        return slow == 1
+    
+    def sumSqrDigits(self, n):
+        return sum([int(i) ** 2 for i in str(n)])
